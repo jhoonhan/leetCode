229d04f2a094dd543b1d2a4502650035b18812b2
Joe Han <jhoon.han@outlook.com>
2023-11-11 23:40:00 +0000

    hashmap

--- easy/twoSum.ts
+++ easy/twoSum.ts
@@ -1,16 +1,46 @@
+///////////// 203ms
 function twoSum(nums: number[], target: number): number[] {
-  let num1: number = -1;
-  let num2: number = -1;
+  // let num1: number = -1;
+  // let num2: number = -1;
 
-  nums.forEach((el, i1, arr) => {
-    arr.forEach((el2, i2) => {
-      if (i1 !== i2 && el + el2 === target) {
-        num1 = i1;
-        num2 = i2;
-      }
-    });
-  });
+  // nums.forEach((el, i1, arr) => {
+  //   arr.forEach((el2, i2) => {
+  //     if (i1 !== i2 && el + el2 === target) {
+  //       num1 = i1;
+  //       num2 = i2;
+  //     }
+  //   });
+  // });
 
-  const result = [num1, num2];
-  return result;
+  // const result = [num1, num2];
+  // return result;
+
+  // Time complexity = O(n^2)
+  // Space complexity = O(1)
+  // for (let i = 0; i < nums.length; i++) {
+  //   for (let j = i + 1; j < nums.length; j++) {
+  //     if (nums[i] + nums[j] === 9) return [i, j];
+  //   }
+  // }
+
+  // Hashmap
+  // Better Time complexity
+  /*
+  map = {1:0, 7:1, 5: 2,   }
+  [1,2,5,9]
+  i = 3
+  value = 9
+  comple = 10 - 9 = 1
+  */
+
+  const map = {};
+  for (let i = 0; i < nums.length; i++) {
+    let value = nums[i];
+    let complementPair = target - value;
+    if (map[complementPair] !== undefined) {
+      return [map[complementPair], i];
+    } else {
+      map[value] = i;
+    }
+  }
 }
