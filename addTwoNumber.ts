dee453d86a1585aa7aaa36345d542a188b2fc014
Joe Han <jhoon.han@outlook.com>
2023-11-13 11:57:51 +0000

    linked list

--- addTwoNumber.ts
+++ addTwoNumber.ts
@@ -7,30 +7,46 @@ class ListNode {
   }
 }
 
-// function getNumberFromList(list: ListNode): number {
-//   let numberString = "";
-
-//   let runner: ListNode | null = list;
-//   while (runner) {
-//     numberString = runner.val.toString() + numberString;
-//     runner = runner.next;
-//   }
-//   return Number(numberString);
-// }
-
 function getNumberFromList(list: ListNode): number {
-  let output = 0;
-  let index = 0;
+  let numberString = "";
 
   let runner: ListNode | null = list;
   while (runner) {
-    output = runner.val * Math.pow(10, index) + output;
+    numberString = runner.val.toString() + numberString;
     runner = runner.next;
-    index++;
   }
-  return output;
+  return Number(numberString);
 }
 
+// function getNumberFromList(list: ListNode): number {
+//   let output = 0;
+//   let index = 0;
+
+//   let runner: ListNode | null = list;
+//   while (runner) {
+//     output += runner.val * Math.pow(10, index);
+//     runner = runner.next;
+//     index++;
+//   }
+//   return output;
+// }
+
 function addTwoNumbers(l1: ListNode, l2: ListNode): ListNode {
-  return l1.next;
+  const n1 = getNumberFromList(l1);
+  123;
+  const n2 = getNumberFromList(l2);
+  234;
+  const sum = (n1 + n2).toString();
+  357;
+
+  let output: ListNode | null = null;
+
+  for (let i = 0; i < sum.length; i++) {
+    const digit = sum[i];
+    3;
+
+    output = new ListNode(Number(digit), output);
+  }
+
+  return output as ListNode;
 }
