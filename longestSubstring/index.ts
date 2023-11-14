0a22942252595f9595fbe83458bc174a591ecbf9
Joe Han <jhoon.han@outlook.com>
2023-11-14 12:12:23 +0000

    worked... but slow

--- longestSubstring/index.ts
+++ longestSubstring/index.ts
@@ -1,18 +1,30 @@
 function lengthOfLongestSubstring(s: string): number {
   let str = "";
+  let previousCount: number = 0;
   let longestCount: number = 0;
 
   for (let i = 0; i < s.length; i++) {
     if (!str.includes(s[i])) {
       str += s[i];
+      previousCount += 1;
     } else {
-      if (str.length > longestCount) {
-        longestCount = str.length;
+      if (previousCount > longestCount) {
+        longestCount = previousCount;
       }
-      str = s[i];
+
+      const charIndex = str.indexOf(s[i]);
+      str = str.slice(charIndex + 1) + s[i];
+      console.log(str);
+      previousCount = str.length;
     }
   }
-  return longestCount;
+
+  const result = Math.max(longestCount, previousCount);
+  console.log(result);
+
+  return result;
 }
 
-lengthOfLongestSubstring("pwwkew");
+lengthOfLongestSubstring(" ");
+// const aang = "01234566789";
+// console.log(aang.slice(1));
