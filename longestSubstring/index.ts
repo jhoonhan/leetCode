5c24e5799c2d3a58dc0bac957bf39c5972bd01ba
Joe Han <jhoon.han@outlook.com>
2023-11-14 11:49:13 +0000

    solved with n^2

--- longestSubstring/index.ts
+++ longestSubstring/index.ts
@@ -1,5 +1,18 @@
-function lengthOfLongestSubstring(s: string): number | void {
+function lengthOfLongestSubstring(s: string): number {
+  let str = "";
+  let longestCount: number = 0;
+
   for (let i = 0; i < s.length; i++) {
-    console.log(s[i]);
+    if (!str.includes(s[i])) {
+      str += s[i];
+    } else {
+      if (str.length > longestCount) {
+        longestCount = str.length;
+      }
+      str = s[i];
+    }
   }
+  return longestCount;
 }
+
+lengthOfLongestSubstring("pwwkew");
