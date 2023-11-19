3607ffd4b4b42c328777b9c4f188000c65d5859f
Joe Han <jhoon.han@outlook.com>
2023-11-19 22:31:57 +0000

    sofast...

---
+++ majorityElement/index.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+class Solution
+{
+
+  /**
+   * @param Integer[] $nums
+   * @return Integer
+   */
+  function majorityElement($nums)
+  {
+    // $map = array();
+    // $largest = 0;
+    // $result = 0;
+
+    // for ($i = 0; $i < count($nums); $i++) {
+    //   $mapCount = 1;
+    //   if (isset($map[$nums[$i]])) {
+    //     $mapCount = $map[$nums[$i]] + 1;
+    //   };
+    //   $map[$nums[$i]] = $mapCount;
+
+    //   if ($mapCount > $largest) {
+    //     $largest = $mapCount;
+    //     $result = $nums[$i];
+    //   }
+    //   // print($mapCount);
+    // }
+    // return $result;
+
+    $nv = array_count_values($nums);
+    arsort($nv);
+    return array_keys($nv)[0];
+  }
+}
+
+
+$solution = new Solution();
+$solution->majorityElement([3, 2, 3, 2, 2]);
