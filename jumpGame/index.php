e74b76de4d82baa730ed5f749eaf4ff69536ff7b
Joe Han <jhoon.han@outlook.com>
2023-11-21 23:12:13 +0000

    tmr

---
+++ jumpGame/index.php
@@ -0,0 +1,47 @@
+<?php
+class Solution
+{
+
+  /**
+   * @param int[] $nums
+   * @return Boolean
+   */
+  function canJump($nums)
+  {
+    $goal = count($nums);
+    $i = 0;
+
+    while ($i < count($nums)) {
+      $slicedArray = array_slice($nums, $i + 1, $nums[$i]);
+      $goal = count(array_slice($nums, $i + 1));
+      if (count($nums) === 1) {
+        return true;
+      }
+
+      if (count($slicedArray) === 0) {
+        echo ('fail');
+        return false;
+      }
+      if ($nums[$i] >= $goal) {
+        echo ('pass');
+        return true;
+      }
+      $max = max($slicedArray);
+      $sofararr = array_slice($nums, 0, $i);
+      var_dump($sofararr);
+      $indeces = array_keys($sofararr, $max);
+      $maxIndex = end($indeces);
+      echo ($maxIndex);
+
+
+
+
+
+      $i = $maxIndex;
+    }
+  }
+}
+
+
+$solution = new Solution();
+$solution->canJump([1, 1, 1, 1]);
