2ab62bc201f6a0e44c9209df5816d53a9bfa467d
Joe Han <jhoon.han@outlook.com>
2023-11-22 23:56:08 +0000

    wow...

--- jumpGame2/index.php
+++ jumpGame2/index.php
@@ -8,39 +8,23 @@ class Solution
    */
   function jump($nums)
   {
-    $goal = count($nums) - 1;
     $count = 0;
-    $i = count($nums) - 1;
-    $potential = 0;
-
-    while ($i > 1) {
-      for ($j = $i; $j > 0; $j--) {
-        if ($nums[$j] + $j >= $i) {
-          $potential = $j;
-          echo ("Updated Poten: $potential | $nums[$potential]<br>");
-        }
-        $i = $potential;
-        echo ("Updated i: $i<br>");
+    $l = $r = 0;
+    while ($r < count($nums) - 1) {
+      $farthest = 0;
+      for ($i = $l; $i <= $r; $i++) {
+        $farthest = max($farthest, $i + $nums[$i]);
       }
+      $l = $r + 1;
+      $r = $farthest;
       $count++;
     }
-    echo ("Count: $count");
-
-
-
-
-
-
-    // if ($goal === 0) {
-    //   return true;
-    // } else {
-    //   return false;
-    // }
+    echo ($count);
     return $count;
   }
 }
 
 
 $solution = new Solution();
-$solution->jump([2, 3, 1, 1, 4]);
-// $solution->jump([1, 1, 1, 1]);
+// $solution->jump([2, 3, 1, 1, 4]);
+$solution->jump([1, 1, 1, 1]);
