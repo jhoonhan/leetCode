a1e239fb4f357e68dff97c02340ae0a5d4ce64bf
Joe Han <jhoon.han@outlook.com>
2023-11-29 20:52:11 +0000

    leggo sleep

--- productOfArrayExceptSelf/index.php
+++ productOfArrayExceptSelf/index.php
@@ -12,18 +12,39 @@ class Solution
   function productExceptSelf($nums)
   {
     $res = [];
+    $res3 = [];
     $counter = 1;
+    $counter2 = 1;
+    $rNums = array_reverse($nums);
     // change it to while loop maybe
-    for ($i = 0; $i < count($nums); $i++) {
-      # code...
+
+    $i = 0;
+    $j = count($nums) - 1;
+    while ($i < count($nums) && $j >= 0) {
+
       if (isset($nums[$i - 1])) {
         $counter = $counter * $nums[$i - 1];
       }
       array_push($res, $counter);
+
+      if (isset($rNums[$j - 1])) {
+        $counter2 = $counter2 * $rNums[$j - 1];
+      }
+      echo ($counter2);
+      echo ('<br>');
+
+      // $res[$i] = $res[$i] * $res
+      array_push($res3, $res[$i] * $counter2);
+
+      $i++;
+      $j--;
     }
-    $counter = 1;
+
 
     print_r($res);
+    echo ('<br>');
+
+    print_r($res3);
     echo ('<br>');
   }
 }
