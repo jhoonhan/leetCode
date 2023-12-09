1da109b6845f7b844b16477ed384f307be2b0ddd
Joe Han <jhoon.han@outlook.com>
2023-12-10 08:10:28 +0900

    imgetting better

--- longestCommonPrefix/index.php
+++ longestCommonPrefix/index.php
@@ -4,12 +4,30 @@ declare(strict_types=1);
 
 class Solution
 {
-
-  /**
-   * @param String[] $strs
-   * @return String
-   */
-  function longestCommonPrefix($strs)
+  function longestCommonPrefix(array $strs): string
   {
+    $prefix = "";
+    $i = 0;
+    while (true) {
+      $prev = "";
+      $tempPrefix = "";
+      foreach ($strs as $str) {
+        if ($prev === "" || $prev === $str[$i]) {
+          $prev = $str[$i];
+          $tempPrefix .= $str[$i];
+        }
+      }
+      if (strlen($tempPrefix) === count($strs)) {
+        $prefix .= $str[$i];
+        $i++;
+      } else {
+        return $prefix;
+      }
+    }
   }
 }
+
+
+$solution = new Solution();
+$solution->longestCommonPrefix(["flower", "flow", "flight"]);
+// $solution->longestCommonPrefix(["flower", "flow", "flight", "aaang"]);
