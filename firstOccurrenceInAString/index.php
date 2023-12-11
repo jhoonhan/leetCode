b570f47759e6078cac83e6b953a8ef24428a7782
Joe Han <jhoon.han@outlook.com>
2023-12-12 00:10:26 +0900

    init

---
+++ firstOccurrenceInAString/index.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+class Solution
+{
+
+
+  function strStr(string $haystack, string $needle): int
+  {
+
+    return 0;
+  }
+}
+
+$solution = new Solution();
+$solution->strStr('sadbutsad', 'sad');
