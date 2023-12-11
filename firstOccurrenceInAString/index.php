4e4f854ab455b5f8e6cc2193615fcb037c925f64
Joe Han <jhoon.han@outlook.com>
2023-12-12 00:27:51 +0900

    first try:

--- firstOccurrenceInAString/index.php
+++ firstOccurrenceInAString/index.php
@@ -4,12 +4,26 @@ declare(strict_types=1);
 
 class Solution
 {
-
-
   function strStr(string $haystack, string $needle): int
   {
+    $acc = "";
+    $i = 0;
+    $j = 0;
+    while ($j < strlen($needle) && $i < strlen($haystack)) {
+      if ($haystack[$i] === $needle[$j]) {
+        $acc .= $haystack[$i];
+        $j++;
+      } else {
+        $acc = "";
+        $j = 0;
+      }
 
-    return 0;
+      if ($acc === $needle) {
+        return $i - (strlen($needle) - 1);
+      }
+
+      $i++;
+    }
   }
 }
 
