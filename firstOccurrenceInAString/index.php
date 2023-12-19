5a53f51c7bffe37075acac5ebf1970dc0cb300b7
Joe Han <jhoon.han@outlook.com>
2023-12-19 13:43:42 +0000

    aang

--- firstOccurrenceInAString/index.php
+++ firstOccurrenceInAString/index.php
@@ -7,37 +7,47 @@ class Solution
   function strStr(string $haystack, string $needle): int
   {
     // O(n^2)
-    for ($i = 0; $i < strlen($haystack); $i++) {
-      $acc = "";
-      if ($haystack[$i] === $needle[0]) {
-        // echo ('<br>');
-        // echo ("First Letter Match: $haystack[$i] | $needle[0]");
-        // echo ('<br>');
+    // for ($i = 0; $i < strlen($haystack); $i++) {
+    //   $acc = "";
+    //   if ($haystack[$i] === $needle[0]) {
+    //     // echo ('<br>');
+    //     // echo ("First Letter Match: $haystack[$i] | $needle[0]");
+    //     // echo ('<br>');
 
-        for ($x = 0; $x < strlen($needle); $x++) {
-          // $index = $x + $i;
-          // echo ("Checking: $haystack[$index] | $needle[$x]");
-          // echo ('<br>');
-          if ($haystack[$x + $i] === $needle[$x]) {
-            $acc .= $haystack[$x + $i];
-            // echo ("ACC updated: $acc");
-            // echo ('<br>');
-          }
+    //     for ($x = 0; $x < strlen($needle); $x++) {
+    //       // $index = $x + $i;
+    //       // echo ("Checking: $haystack[$index] | $needle[$x]");
+    //       // echo ('<br>');
+    //       if ($haystack[$x + $i] === $needle[$x]) {
+    //         $acc .= $haystack[$x + $i];
+    //         // echo ("ACC updated: $acc");
+    //         // echo ('<br>');
+    //       }
+    //     }
+    //     // echo ("FINAL: $acc");
+    //     // echo ('<br>');
+    //     if ($acc === $needle) {
+    //       // echo ("HOOYA");
+    //       // echo ($i);
+    //       return $i;
+    //     }
+    //   }
+    // }
+
+    // why...?
+    for ($i = 0; $i < strlen($haystack) + 1 - strlen($needle); $i++) {
+      for ($j = 0; $j < strlen($needle); $j++) {
+        if ($haystack[$i + $j] !== $needle[$j]) {
+          break;
         }
-        // echo ("FINAL: $acc");
-        // echo ('<br>');
-        if ($acc === $needle) {
-          // echo ("HOOYA");
-          // echo ($i);
+        if ($j === strlen($needle) - 1) {
           return $i;
         }
       }
     }
-
-    // echo ("Failed");
     return -1;
   }
 }
 
 $solution = new Solution();
-$solution->strStr('a', 'a');
+$solution->strStr('sadbu', 'sad');
