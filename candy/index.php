79c928b266941e3964041cc7ba77d5f3b469c657
Joe Han <jhoon.han@outlook.com>
2023-12-19 18:32:50 +0000

    workx but slow

--- candy/index.php
+++ candy/index.php
@@ -8,12 +8,10 @@ class Solution
 
   function checkPrev($ratings, $start)
   {
-    echo ("aaang");
-
-    if (isset($ratings[$start - 1]) && $ratings[$start] > $ratings[$start - 1]) {
-      $this->arr[$start - 1] += 1;
-      echo ("Prev: ");
-      echo ($this->arr[$start - 1]);
+    if (isset($ratings[$start - 1]) && $this->arr[$start] === $this->arr[$start - 1]) {
+      if ($ratings[$start - 1] > $ratings[$start]) {
+        $this->arr[$start - 1] += 1;
+      }
       $this->checkPrev($ratings, $start - 1);
     }
   }
@@ -22,44 +20,31 @@ class Solution
   {
     $i = 0;
     $this->arr = array_fill(0, count($ratings), 1);
-    // print_r($this->arr);
-
-
 
     while ($i < count($ratings)) {
-      // Check left
+      $changed = false;
       if (isset($ratings[$i - 1]) && $ratings[$i - 1] < $ratings[$i]) {
         $this->arr[$i] = $this->arr[$i - 1] + 1;
-
-        echo ("Left: ");
-        echo ($this->arr[$i]);
-        echo ('<br>');
-      } elseif (isset($ratings[$i + 1]) && $ratings[$i] > $ratings[$i + 1]) {
-        // Check right
-        $this->arr[$i] += 1;
-        echo ("Right: ");
-        echo ($this->arr[$i]);
-        echo ('<br>');
-        // Check prev
-        $this->checkPrev($ratings, $i);
-      } else {
-        echo ("None: ");
-        echo ($this->arr[$i]);
-        echo ('<br>');
-        $this->arr[$i] = 1;
+        $changed = true;
       }
+
+      if ($changed === false && isset($ratings[$i + 1]) && $ratings[$i] > $ratings[$i + 1]) {
+        $this->arr[$i] += 1;
+        $this->checkPrev($ratings, $i);
+        $changed = true;
+      }
+
+      if ($changed === false) {
+        $this->arr[$i] = 1;
+        $changed = true;
+      }
+
       $i++;
     }
 
-    // print_r($this->arr);
-    foreach ($this->arr as $key => $value) {
-      echo ($value);
-      echo (" ");
-    }
-
-    return 0;
+    return array_sum($this->arr);
   }
 }
 
 $solution = new Solution();
-$solution->candy([0, 1, 2, 1, 7, 3, 0, 1, 2]);
+$solution->candy([29, 51, 87, 87, 72, 12]);
