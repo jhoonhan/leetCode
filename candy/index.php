42817c3bbc229153028c1704788f1e0a12af8639
Joe Han <jhoon.han@outlook.com>
2023-12-20 09:53:36 +0000

    workx

--- candy/index.php
+++ candy/index.php
@@ -4,47 +4,25 @@ declare(strict_types=1);
 
 class Solution
 {
-  private $arr = [];
-
-  function checkPrev($ratings, $start)
-  {
-    if (isset($ratings[$start - 1]) && $this->arr[$start] === $this->arr[$start - 1]) {
-      if ($ratings[$start - 1] > $ratings[$start]) {
-        $this->arr[$start - 1] += 1;
-      }
-      $this->checkPrev($ratings, $start - 1);
-    }
-  }
-
   function candy(array $ratings): int
   {
-    $i = 0;
-    $this->arr = array_fill(0, count($ratings), 1);
+    $nums = array_fill(0, count($ratings), 1);
 
-    while ($i < count($ratings)) {
-      $changed = false;
-      if (isset($ratings[$i - 1]) && $ratings[$i - 1] < $ratings[$i]) {
-        $this->arr[$i] = $this->arr[$i - 1] + 1;
-        $changed = true;
+    for ($i = 1; $i < count($ratings); $i++) {
+      if ($ratings[$i] > $ratings[$i - 1]) {
+        $nums[$i] = $nums[$i - 1] + 1;
       }
-
-      if ($changed === false && isset($ratings[$i + 1]) && $ratings[$i] > $ratings[$i + 1]) {
-        $this->arr[$i] += 1;
-        $this->checkPrev($ratings, $i);
-        $changed = true;
+    }
+    for ($i = count($ratings) - 2; $i >= 0; $i--) {
+      if ($ratings[$i] > $ratings[$i + 1]) {
+        $nums[$i] = max($nums[$i], $nums[$i + 1] + 1);
       }
-
-      if ($changed === false) {
-        $this->arr[$i] = 1;
-        $changed = true;
-      }
-
-      $i++;
     }
 
-    return array_sum($this->arr);
+    return array_sum($nums);
   }
 }
 
+
 $solution = new Solution();
-$solution->candy([29, 51, 87, 87, 72, 12]);
+$solution->candy([6, 1, 2, 0, 4, 5]);
