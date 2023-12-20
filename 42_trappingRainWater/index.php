69d485ea799e4a555ab9a2e4b1251a8d4ddab4dc
Joe Han <jhoon.han@outlook.com>
2023-12-20 12:02:38 +0000

    leggo

---
+++ 42_trappingRainWater/index.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+class Solution {
+
+  /**
+   * @param Integer[] $height
+   * @return Integer
+   */
+  function trap($height) {
+      
+  }
+}
+
+$solution = new Solution(); 
+$solution->();
