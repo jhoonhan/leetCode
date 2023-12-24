9f8aea0fd423c26699bc8b7d443ddcaaa9521ce7
Joe Han <jhoon.han@outlook.com>
2023-12-24 14:58:44 +0000

    wow...

--- minimumSizeSubarraySum/index.php
+++ minimumSizeSubarraySum/index.php
@@ -10,10 +10,16 @@ class Solution
     $total = 0;
     $res = count($nums) + 1;
 
+    // Right always moves
     for ($r = 0; $r < count($nums); $r++) {
+      // Skipping Acc - L jump to Acc - L + R
       $total += $nums[$r];
+      // Only when total is larger
       while ($total >= $target) {
+        // Persist to the res;
         $res = min($r - $l + 1, $res);
+
+        // Move the left pointer
         $total -= $nums[$l];
         $l++;
       }
