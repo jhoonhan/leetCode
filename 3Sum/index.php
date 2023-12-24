2191a0363bebee9c93125ca750c959546cc8caa0
Joe Han <jhoon.han@outlook.com>
2023-12-24 10:56:16 +0000

    dang

--- 3Sum/index.php
+++ 3Sum/index.php
@@ -10,42 +10,19 @@ class Solution
   {
     sort($nums);
     $res = [];
-    echo (json_encode($nums));
-    echo ('<br>');
-    echo ('<br>');
-
     for ($i = 0; $i < count($nums); $i++) {
       $l = $i + 1;
       $r = count($nums) - 1;
-      echo ("CHECKING: $i :: $nums[$i]");
-      echo ('<br>');
-
-
       if ($i > 0 && $nums[$i] === $nums[$i - 1]) {
         continue;
       }
-
       while ($l < $r) {
         $target = $nums[$i] + $nums[$l] + $nums[$r];
-        echo ("T: $target");
-        echo ('<br>');
-
-        echo ("$nums[$i] | $nums[$l] | $nums[$r]");
-        echo ('<br>');
-        echo ('<br>');
-        // Target is larger
         if ($target > 0) {
           $r--;
-        }
-        // Target is smaller
-        elseif ($target < 0) {
+        } elseif ($target < 0) {
           $l++;
-        }
-        // Target is equal
-        else {
-          echo ("Match made $nums[$i], $nums[$l], $nums[$r]");
-          echo ('<br>');
-          echo ('<br>');
+        } else {
           $res[] = [$nums[$i], $nums[$l], $nums[$r]];
           $l++;
           while ($nums[$l] === $nums[$l - 1] and $l < $r) {
@@ -54,11 +31,6 @@ class Solution
         }
       }
     }
-
-    echo ('<br>');
-    echo ('<br>');
-    echo (json_encode($res));
-
     return $res;
   }
 }
