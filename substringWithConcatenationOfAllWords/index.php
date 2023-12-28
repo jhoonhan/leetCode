75aa3c19c1759a26d186b8067fe208f11a6a587c
Joe Han <jhoon.han@outlook.com>
2023-12-28 11:20:14 +0000

    wow

--- substringWithConcatenationOfAllWords/index.php
+++ substringWithConcatenationOfAllWords/index.php
@@ -4,80 +4,57 @@ declare(strict_types=1);
 
 class Solution
 {
-
-
   function findSubstring(string $s, array $words): array
   {
-    $wordLegnth = strlen($words[0]);
-    $success = [];
+    // $wordLegnth = strlen($words[0]);
+    // $success = [];
+    // for ($i = 0; $i <= strlen($s) - ($wordLegnth * count($words)); $i++) {
+    //   $substr = substr($s, $i, $wordLegnth);
+    //   $brute = $words;
+    //   if (in_array($substr, $brute)) {
+    //     for ($j = $i; $j <= ($wordLegnth * count($words)) + $i; $j += $wordLegnth) {
+    //       $nextWord = substr($s, $j, $wordLegnth);
+    //       $key = array_search($nextWord, $brute);
+    //       if (in_array($nextWord, $brute)) {
+    //         unset($brute[$key]);
+    //         if (count($brute) === 0) {
+    //           $success[] = $i;
+    //           break;
+    //         }
+    //       } else {
+    //         break;
+    //       }
+    //     }
+    //   }
+    // }
+    // echo (json_encode($success));
+    // return $success;
 
+    $result = [];
+    if (count($words) < 1) return $result;
+    $strLength  = strlen($s);
+    $wordLength = strlen($words[0]);
+    $subLength  = $wordLength * count($words);
 
-    for ($i = 0; $i <= strlen($s) - ($wordLegnth * count($words)); $i++) {
-      // Check first 3
-      $substr = substr($s, $i, $wordLegnth);
+    sort($words);
 
+    for ($i = 0; $i <= ($strLength - $subLength); $i++) {
+      $strToArr = str_split(substr($s, $i, $subLength), $wordLength);
+      sort($strToArr);
+      echo (json_encode($strToArr));
+      echo ('<br>');
 
-      // echo ("$i :: $substr");
-      // echo ('<br>');
-
-      // If yes
-      $brute = $words;
-      if (in_array($substr, $brute)) {
-        for ($j = $i; $j <= ($wordLegnth * count($words)) + $i; $j += $wordLegnth) {
-          # code...
-          echo ($j);
-          echo (" | ");
-          echo ($wordLegnth * count($words) + $wordLegnth);
-
-
-          echo ('<br>');
-
-          $nextWord = substr($s, $j, $wordLegnth);
-          echo ("$i-$j :: $nextWord");
-          echo ('<br>');
-          echo (json_encode($brute));
-          echo ('<br>');
-          var_dump(in_array($nextWord, $brute));
-
-
-
-          $key = array_search($nextWord, $brute);
-          if (in_array($nextWord, $brute)) {
-            echo ("AANG");
-            echo ('<br>');
-            unset($brute[$key]);
-            echo (json_encode($brute));
-            echo ('<br>');
-            if (count($brute) === 0) {
-              echo ("SUCCESS!");
-              echo ('<br>');
-              $success[] = $i;
-              break;
-            }
-          } else {
-            // echo ("Moving On");
-            echo ('<br>');
-            echo ('<br>');
-            break;
-          }
-        }
-      }
-      // If no
-      else {
-        // echo ("Moving On");
-        echo ('<br>');
-        echo ('<br>');
+      if (
+        count($words) === count($strToArr)
+        && empty(array_diff_assoc($strToArr, $words))
+      ) {
+        $result[] = $i;
       }
     }
-
-
-    echo (json_encode($success));
-    echo ('<br>');
-
-    return $success;
+    return $result;
   }
 }
 
 $solution = new Solution();
-$solution->findSubstring("barfoothefoobarman", ["foo", "bar"]);
-// $solution->findSubstring("wordgoodgoodgoodbestword", ["word", "good", "best", "good"]);
+// $solution->findSubstring("barfoothefoobarman", ["foo", "bar"]);
+$solution->findSubstring("wordgoodgoodgoodbestword", ["word", "good", "best", "good"]);
