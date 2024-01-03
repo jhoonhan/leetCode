803710f3fe7e2650d9296f9de914c997261addb5
Joe Han <jhoon.han@outlook.com>
2024-01-03 00:33:24 +0000

    why

--- minimumWindowSubstring/index.php
+++ minimumWindowSubstring/index.php
@@ -10,69 +10,93 @@ class Solution
       return "";
     }
 
-    $tLength = strlen($t);
-    $l = 0;
-    $r = $tLength;
-    $sChars = str_split($s);
     $tChars = str_split($t);
-    $res = [];
+    $sChars = str_split($s);
 
     // Create hashmap
-    $map = array_fill_keys($tChars, 0);
+    $window = array_fill_keys($tChars, 0);
+    $countT = array_fill_keys($tChars, 0);
     foreach ($tChars as $value) {
-      $map[$value]++;
+      $countT[$value]++;
     }
-    // ksort($map);
 
+    $have = 0;
+    $need = count($countT);
+    $res = [-1, -1];
+    $resLen = INF;
+    $l = 0;
 
-    while ($l < $r && $r < strlen($s) + 1) {
-      $subStr = array_slice($sChars, $l, $r - $l);
-      $window = array_fill_keys($tChars, 0);
-      $has = 0;
-      foreach ($subStr as $value) {
-        if (in_array($value, $tChars)) {
-          $window[$value]++;
-          if ($window[$value] <= $map[$value]) {
-            $has++;
-          }
-        }
+    for ($r = 0; $r < strlen($s) - 1; $r++) {
+      $c = $s[$r];
+      $window[$c] = 1 + ($window[$c] ?? 0);
+
+      if (array_key_exists($c, $countT) && $window[$c] === $countT[$c]) {
+        $have++;
       }
-      echo ("$l and $r");
-      echo ('<br>');
 
-      echo (":: ");
-      echo (implode('', $subStr));
-      echo (" | ");
+      while ($have === $need) {
+        if (($r - $l + 1) < $resLen) {
+          $res = [$l, $r];
+          $resLen = ($r - $l + 1);
+        }
+        $window[$s[$l]]--;
 
-      echo ($has);
-      echo ('<br>');
-
-
-      // If contains
-      if ($has === strlen($t)) {
-        echo ('good');
-        echo ('<br>');
+        if (array_key_exists($s[$l], $countT) && $window[$s[$l]] < $countT[$s[$l]]) {
+          $have--;
+        }
         $l++;
-        if (count($subStr) < count($res) || count($res) === 0) {
-          $res = $subStr;
-        }
       }
-      // If not
-      else {
-        echo ('bad');
-        echo ('<br>');
-        $r++;
-      }
-    }
-
-    echo ('<br>');
-    echo ('<br>');
-    $aang = implode('', $res);
-    echo ("RESULT: $aang");
+    };
 
 
 
-    return implode('', $res);
+    // while ($l < $r && $r < strlen($s) + 1) {
+    //   $subStr = array_slice($sChars, $l, $r - $l);
+    //   $window = array_fill_keys($tChars, 0);
+    //   $has = 0;
+    //   foreach ($subStr as $value) {
+    //     if (in_array($value, $tChars)) {
+    //       $window[$value]++;
+    //       if ($window[$value] <= $map[$value]) {
+    //         $has++;
+    //       }
+    //     }
+    //   }
+    //   echo ("$l and $r");
+    //   echo ('<br>');
+
+    //   echo (":: ");
+    //   echo (implode('', $subStr));
+    //   echo (" | ");
+
+    //   echo ($has);
+    //   echo ('<br>');
+
+
+    //   // If contains
+    //   if ($has === strlen($t)) {
+    //     echo ('good');
+    //     echo ('<br>');
+    //     $l++;
+    //     if (count($subStr) < count($res) || count($res) === 0) {
+    //       $res = $subStr;
+    //     }
+    //   }
+    //   // If not
+    //   else {
+    //     echo ('bad');
+    //     echo ('<br>');
+    //     $r++;
+    //   }
+    // }
+
+
+
+
+
+    echo (implode('', array_slice($sChars, $l, $r - $l)));
+
+    return implode('', array_slice($sChars, $l, $r - $l));
   }
 }
 
