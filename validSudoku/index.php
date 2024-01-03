2848638e3496709527950022e88923fd51c89ab8
Joe Han <jhoon.han@outlook.com>
2024-01-03 13:38:45 +0000

    faster

--- validSudoku/index.php
+++ validSudoku/index.php
@@ -7,26 +7,19 @@ class Solution
   function isValidSudoku(array $board): bool
   {
     $res = true;
-
     // check by row
     foreach ($board as $row) {
       $map = array();
       foreach ($row as $value) {
-
         if (is_numeric($value)) {
-          // echo ($value);
-          // echo ('<br>');
           if ($value > 9 || $value < 1) {
-            echo ("Failed on Row");
-            echo ('<br>');
             return false;
           }
-
-          $map[$value] = 1 + ($map[$value] ?? 0);
-          if ($map[$value] > 1) {
-            echo ("Failed on Row");
-            echo ('<br>');
+          if (in_array($value, $map)) {
+            echo ("FAILED ON ROW");
             return false;
+          } else {
+            $map[] = $value;
           }
         }
       }
@@ -36,61 +29,39 @@ class Solution
       $map = array();
       for ($j = 0; $j < count($board[0]); $j++) {
         if (is_numeric($board[$j][$i])) {
-          // $curr = $board[$j][$i];
-          // echo ("$i :: $j :: $curr");
-          // echo ('<br>');
-
-          $map[$board[$j][$i]] = 1 + ($map[$board[$j][$i]] ?? 0);
-
-          if ($map[$board[$j][$i]] > 1) {
-            echo ("Failed on Column");
-            echo ('<br>');
+          if (in_array($board[$j][$i], $map)) {
+            echo ("FAILED on COL");
             return false;
+          } else {
+            $map[] = $board[$j][$i];
           }
         }
       }
     }
-
-
-
     // check by 3x3
     $map = array_fill(0, 9, []);
     $rowNumber = 0;
-
     for ($i = 0; $i < count($board); $i++) {
       $chunks = array_chunk($board[$i], 3);
       for ($j = 0; $j < count($chunks); $j++) {
         $rowNumber = (int)($i / 3);
         $boxNumber = $rowNumber * 3 + $j;
-        // echo ("ROW# : $rowNumber");
-        // echo ("BOX# : $boxNumber");
-        // echo ('<br>');
         $map[$boxNumber] = array_merge($map[$boxNumber], $chunks[$j]);
       }
     }
-
     foreach ($map as $board) {
       $hash = array();
       foreach ($board as $value) {
         if (is_numeric($value)) {
-          $hash[$value] = 1 + ($hash[$value] ?? 0);
-          if ($hash[$value] > 1) {
-            echo ("Failed on 3X3");
-            echo ('<br>');
+          if (in_array($value, $hash)) {
+            echo ("FAILED ON 3X#");
             return false;
+          } else {
+            $hash[] = $value;
           }
         }
       }
-
-      echo (json_encode($board));
-      echo ('<br>');
     }
-
-
-
-
-
-
 
     var_dump($res);
     return $res;
@@ -99,5 +70,5 @@ class Solution
 
 $solution = new Solution();
 $solution->isValidSudoku([
-  ["8", "3", ".", ".", "7", ".", ".", ".", "."], ["6", ".", ".", "1", "9", "5", ".", ".", "."], [".", "9", "8", ".", ".", ".", ".", "6", "."], ["8", ".", ".", ".", "6", ".", ".", ".", "3"], ["4", ".", ".", "8", ".", "3", ".", ".", "1"], ["7", ".", ".", ".", "2", ".", ".", ".", "6"], [".", "6", ".", ".", ".", ".", "2", "8", "."], [".", ".", ".", "4", "1", "9", ".", ".", "5"], [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+  ["5", "3", ".", ".", "7", ".", ".", ".", "."], ["6", ".", ".", "1", "9", "5", ".", ".", "."], [".", "9", "8", ".", ".", ".", ".", "6", "."], ["8", ".", ".", ".", "6", ".", ".", ".", "3"], ["4", ".", ".", "8", ".", "3", ".", ".", "1"], ["7", ".", ".", ".", "2", ".", ".", ".", "6"], [".", "6", ".", ".", ".", ".", "2", "8", "."], [".", ".", ".", "4", "1", "9", ".", ".", "5"], [".", ".", ".", ".", "8", ".", ".", "7", "9"]
 ]);
