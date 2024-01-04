623e64895c626ccaf1d066349cdfd0a5c77339a4
Joe Han <jhoon.han@outlook.com>
2024-01-04 09:52:27 +0000

    lets go

--- spiralMatrix/index.php
+++ spiralMatrix/index.php
@@ -5,15 +5,23 @@ declare(strict_types=1);
 
 class Solution
 {
-
-  /**
-   * @param Integer[][] $matrix
-   * @return Integer[]
-   */
-  function spiralOrder($matrix)
+  function spiralOrder(array $matrix): array
   {
+    $totalCount = count($matrix[0]) * count($matrix);
+
+    echo ($totalCount);
+
+    $minV = 0;
+    $maxV = count($matrix[0]);
+    $minH = 0;
+    $maxH = $maxV;
+    $res = (array)[];
+
+    // while(count($res) !== count())
+
+    return $res;
   }
 }
 
 $solution = new Solution();
-$solution->spiralOrder([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
+$solution->spiralOrder([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]);
