003cc72784216348aee7fadcd77cee96926dfc87
Joe Han <jhoon.han@outlook.com>
2024-01-04 09:18:07 +0000

    init

---
+++ spiralMatrix/index.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+
+class Solution
+{
+
+  /**
+   * @param Integer[][] $matrix
+   * @return Integer[]
+   */
+  function spiralOrder($matrix)
+  {
+  }
+}
+
+$solution = new Solution();
+$solution->spiralOrder([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
