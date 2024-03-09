336a086b7dd5faf461dbef287f0d8e96718a04b8
Joe Han <jhoon.han@outlook.com>
2024-03-09 11:38:00 +0000

    leggo

---
+++ countCompleteTreeNodes/index.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution
+{
+
+	/**
+	 * @param   TreeNode  $root
+	 *
+	 * @return Integer
+	 */
+	function countNodes($root)
+	{
+	}
+}
