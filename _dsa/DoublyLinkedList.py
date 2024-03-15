ee7444e8f518dcee1e55806d4aa85f0736ac92a3
Joe Han <jhoon.han@outlook.com>
2024-03-15 11:00:35 +0000

    working

--- _dsa/DoublyLinkedList.py
+++ _dsa/DoublyLinkedList.py
@@ -11,3 +11,29 @@ class DoublyLinkedList:
         self.head = new_node
         self.tail = new_node
         self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+
+        self.length += 1
+
+        return True
+
+
+aang = DoublyLinkedList(1)
+aang.append(2)
+
+aang.print_list()
