a584bf33d8a341bc26a6b3d9990f595d10f07512
Joe Han <jhoon.han@outlook.com>
2024-03-18 22:04:31 +0000

    leggo

---
+++ convertSortedListToBST/index.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedListToBST(self, head):
+        print("aang")
+
+
+sol = Solution()
+sol.sortedListToBST([-10, -3, 0, 5, 9])
