3f91125f2996ea15db6253047f4d736be4eb2c9b
Joe Han <jhoon.han@outlook.com>
2024-03-18 22:23:18 +0000

    lord...

--- convertSortedListToBST/index.py
+++ convertSortedListToBST/index.py
@@ -1,25 +1,39 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
 
-    def sortedListToBST(self, head):
-        if len(head) == 0:
-            return []
-        root = head[0]
-        def r_insert(currentNode, value):
-            
-            
+    def __r_insert(self, currentNode, value):
+        if currentNode == None:
+            return TreeNode(value)
+        if currentNode.value > value:
+            currentNode.left = self.__r_insert(currentNode.left, value)
+        if currentNode.value < value:
+            currentNode.right = self.__r_insert(currentNode.right, value)
+        return currentNode
 
-        
+    def r_insert(self, currentNode, value):
+        self.__r_insert(currentNode, value)
+
+    def sortedListToBST(self, head):
+        if head == None:
+            return []
+        temp = head
+        while temp:
+            print(temp.val)
+            temp = temp.next
 
 
 sol = Solution()
