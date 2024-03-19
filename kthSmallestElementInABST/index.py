40678f2b4268e4ea9d9de845020168880c4bb71e
Joe Han <jhoon.han@outlook.com>
2024-03-19 21:21:59 +0000

    aang

--- kthSmallestElementInABST/index.py
+++ kthSmallestElementInABST/index.py
@@ -6,4 +6,35 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        
+        # res = []
+
+        # if root == None:
+        #     return None
+
+        # def __traverse(curr_node):
+        #     if curr_node.left is not None:
+        #         __traverse(curr_node.left)
+        #     # In order
+        #     res.append(curr_node.val)
+        #     if curr_node.right is not None:
+        #         __traverse(curr_node.right)
+
+        # __traverse(root)
+
+        # return res[k - 1]
+
+        n = 0
+        stack = []
+        cur = root
+
+        while cur and stack:
+            while cur is not None:
+                stack.append(cur)
+                cur = cur.left
+
+            cur = stack.pop()
+            n += 1
+            if n == k:
+                return cur.val
+
+            cur = cur.right
