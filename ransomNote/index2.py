f56bbddebec219e824031e557178c97932bb102c
Joe Han <jhoon.han@outlook.com>
2024-03-21 23:42:01 +0000

    workx

--- ransomNote/index2.py
+++ ransomNote/index2.py
@@ -1,8 +1,28 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        print("aang")
+        map = {}
+        for char in magazine:
+            if char not in map:
+                map[char] = 1
+            else:
+                map[char] += 1
+
+        for char in ransomNote:
+            if char in map:
+                if map[char] == 0:
+                    print("failed")
+
+                    return False
+                else:
+                    map[char] -= 1
+            else:
+                print("failed")
+
+                return False
+        print("pass")
+
+        return True
 
 
-sol = Solution
-
-sol.canConstruct("aa", "aab")
+sol = Solution()
+sol.canConstruct("aac", "acab")
