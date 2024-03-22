c8f79cb06323d420cdd1bb95f8de7e1ad3906b59
Joe Han <jhoon.han@outlook.com>
2024-03-22 14:34:48 +0000

    lets go

---
+++ summaryRanges/index.py
@@ -0,0 +1,9 @@
+class Solution:
+    def summaryRanges(self, nums: list) -> list:
+        res = []
+        l = 0
+        for r in range(len(nums)):
+            print(r)
+
+
+Solution().summaryRanges([0, 1, 2, 4, 5, 7])
