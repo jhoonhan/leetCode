91ba98de8c85592f154d095f1e35dca6c3c96b4e
Joe Han <jhoon.han@outlook.com>
2024-03-22 21:40:24 +0000

    complete

--- simplifyPath/index.py
+++ simplifyPath/index.py
@@ -2,7 +2,20 @@ class Solution:
     def simplifyPath(self, path: str) -> str:
         stack = []
         new_path = path + "/"
-        print(new_path)
+        curr = ""
+        for i in range(len(new_path)):
+            if new_path[i] == "/":
+                if curr == "..":
+                    if len(stack) > 0:
+                        stack.pop()
+                elif curr != "." and curr != "":
+                    stack.append(curr)
+                curr = ""
+            else:
+                curr = curr + new_path[i]
+        res = "/" + "/".join(stack)
+
+        return res
 
 
-Solution().simplifyPath("/home/")
+Solution().simplifyPath("/../abc///./ab")
