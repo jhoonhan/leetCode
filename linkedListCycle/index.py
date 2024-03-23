d6dca70673029f52004fd8852afd73f822beb53f
Joe Han <jhoon.han@outlook.com>
2024-03-23 09:19:20 +0000

    floyd

--- linkedListCycle/index.py
+++ linkedListCycle/index.py
@@ -4,6 +4,16 @@
 #         self.val = x
 #         self.next = None
 
+
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        
+        # Floyd Cycle Detections
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+
+        return False
