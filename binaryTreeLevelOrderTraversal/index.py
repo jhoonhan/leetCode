1f197a0c5c31992038f5d69dba2f4c5bd4e3378c
Joe Han <jhoon.han@outlook.com>
2024-03-27 14:37:38 +0000

    that was impressive

---
+++ binaryTreeLevelOrderTraversal/index.py
@@ -0,0 +1,31 @@
+import collections
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root == None:
+            return None
+        q = collections.deque()
+        q.append(root)
+        level = [[root.val]]
+        while q:
+            q_len = len(q)
+            res = []
+            for _ in range(q_len):
+                cur = q.popleft()
+                if cur.left:
+                    q.append(cur.left)
+                    res.append(cur.left.val)
+                if cur.right:
+                    q.append(cur.right)
+                    res.append(cur.right.val)
+            if res:
+                level.append(res)
+
+        return level
