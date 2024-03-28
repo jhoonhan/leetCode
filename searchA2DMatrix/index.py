f188902c8729ea398c32f9f22a8bf587c7e4f8e7
Joe Han <jhoon.han@outlook.com>
2024-03-28 20:40:51 +0000

    yes

--- searchA2DMatrix/index.py
+++ searchA2DMatrix/index.py
@@ -1,9 +1,11 @@
 class Solution:
     def searchMatrix(self, matrix: list, target: int) -> bool:
         l = 0
-        r = len(matrix[0])
+        r = len(matrix[0]) - 1
         t = 0
-        b = len(matrix)
+        b = len(matrix) - 1
+
+        # Find row
         while t <= b:
             mid = (t + b) // 2
 
@@ -11,11 +13,22 @@ class Solution:
                 return True
 
             if target > matrix[mid][0]:
+                t = mid + 1
+            else:
+                b = mid - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+
+            if matrix[b][mid] == target:
+                return True
+
+            if target > matrix[b][mid]:
                 l = mid + 1
             else:
                 r = mid - 1
 
-        print(t)
+        return False
 
 
-Solution().searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 3)
+Solution().searchMatrix([[1]], 11)
