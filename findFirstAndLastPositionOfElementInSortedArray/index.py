5c13af1e83425f3d044d62de0503eefd108eac3d
Joe Han <jhoon.han@outlook.com>
2024-03-29 10:38:36 +0000

    fucking workx lol

--- findFirstAndLastPositionOfElementInSortedArray/index.py
+++ findFirstAndLastPositionOfElementInSortedArray/index.py
@@ -1,3 +1,46 @@
 class Solution:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
-        
+    def searchRange(self, nums: list, target: int) -> list:
+        l = 0
+        r = len(nums) - 1
+        res = [-1, -1]
+
+        # find smallest
+        while l <= r:
+            m = l + ((r - l) // 2)
+            # reg BS
+            if nums[m] > target:
+                r = m - 1
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                # When position is met
+                if nums[l] == target:
+                    res[0] = l
+                    break
+                if l < m:
+                    r = m
+
+        # Find largest
+        l = 0
+        r = len(nums) - 1
+        while l <= r:
+
+            m = l + ((r - l) // 2)
+            # reg BS
+            if nums[m] > target:
+                r = m - 1
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                # When position is met
+                if nums[r] == target:
+                    res[1] = r
+                    break
+                if r > m:
+                    l = m
+                    r -= 1
+        print(res)
+        return res
+
+
+Solution().searchRange([5, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8, 9, 10], 88)
