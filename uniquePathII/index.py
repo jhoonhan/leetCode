6ffd73282eb4bb6af7c52054de1fb66d53753ef9
Joe Han <jhoon.han@outlook.com>
2024-04-03 22:57:59 +0100

    wtf

--- uniquePathII/index.py
+++ uniquePathII/index.py
@@ -13,7 +13,6 @@ class Solution:
 
         for r in range(row - 1, -1, -1):
             for c in range(col - 1, -1, -1):
-                res = 0
                 # If its a block
                 if dp[r][c] == None:
                     dp[r][c] = 0
@@ -23,23 +22,39 @@ class Solution:
 
         return dp[0][0]
 
-    # DFS brute force
     def uniquePathsWithObstacles(self, obstacleGrid: list) -> int:
         row = len(obstacleGrid)
         col = len(obstacleGrid[0])
+        dp = [0] * col
+        dp[-1] = 1
+
+        for r in range(row - 1, -1, -1):
+            for c in range(col - 1, -1, -1):
+                if obstacleGrid[r][c]:
+                    dp[c] = 0
+                elif c + 1 < col:
+                    dp[c] = dp[c] + dp[c + 1]
+        return dp[0]
+
+    # DFS with cache
+    def uniquePathsWithObstacles3(self, obstacleGrid: list) -> int:
+        row = len(obstacleGrid)
+        col = len(obstacleGrid[0])
+        dp = {(row - 1, col - 1): 1}
 
         def dfs(r, c):
 
-            if r < 0 or r > row - 1 or c < 0 or c > col - 1 or obstacleGrid[r][c] == 1:
+            if r == row or c == col or obstacleGrid[r][c]:
                 return 0
 
-            if r == row - 1 and c == col - 1 and obstacleGrid[r][c] != 1:
-                return 1
+            if (r, c) in dp:
+                return dp[(r, c)]
 
             bottom = dfs(r + 1, c)
             right = dfs(r, c + 1)
+            dp[(r, c)] = bottom + right
 
-            return bottom + right
+            return dp[(r, c)]
 
         return dfs(0, 0)
 
