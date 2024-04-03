1b8121fb234f22600627fa7f308ddf59ebf6bd18
Joe Han <jhoon.han@outlook.com>
2024-04-03 12:48:36 +0100

    recursion is hard

---
+++ triangle/index.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimumTotal(self, triangle: list) -> int:
+
+        def dfs(node, acc, level):
+            print("aang")
+
+
+Solution().minimumTotal([[-1], [2, 3], [1, -1, -3]])
