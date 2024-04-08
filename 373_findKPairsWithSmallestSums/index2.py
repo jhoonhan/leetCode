adb1aa17e23dc79e7d87f13687648fcb5cdac567
Joe Han <jhoon.han@outlook.com>
2024-04-08 23:34:30 +0100

    leggo

---
+++ 373_findKPairsWithSmallestSums/index2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def kSmallestPairs(self, nums1: list, nums2: list, k: int) -> list:
+        print("aang")
+        res = []
+        heap = []
+
+
+Solution().kSmallestPairs([1, 7, 11], [2, 4, 6], 3)
