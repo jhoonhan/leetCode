a42eba5c9c2f52433f7ab818e46073f966cf1344
Joe Han <jhoon.han@outlook.com>
2024-04-08 23:49:40 +0100

    tackling hardest

---
+++ 502_IPO/index.py
@@ -0,0 +1,9 @@
+import heapq
+
+
+class Solution:
+    def findMaximizedCapital(self, k: int, w: int, profits: list, capital: list) -> int:
+        print("aang")
+
+
+Solution().findMaximizedCapital(2, 0, [1, 2, 3], [0, 1, 1])
