126c46b8ea9918248565d6c8c5d491edf283d75d
Joe Han <jhoon.han@outlook.com>
2024-04-09 17:48:31 +0100

    hmmm

--- 139_wordBreak/index2.py
+++ 139_wordBreak/index2.py
@@ -1,6 +1,16 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: list) -> bool:
-        print("fucking wrong problem")
+        dp = [False] * (len(s) + 1)
+        dp[-1] = True
+
+        for i in range(len(s) - 1, -1, -1):
+            print(i)
+            for word in wordDict:
+                if (i + len(word)) <= len(s) and s[i : i + len(word)] == word:
+                    dp[i] = dp[i + len(word)]
+                if dp[i]:
+                    break
+        return dp[0]
 
 
-Solution().wordBreak("leetcode")
+Solution().wordBreak("leetcode", ["leet", "code"])
