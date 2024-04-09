b868a6ae70c533782e59fbd2b500e2dbf4cde8f8
Joe Han <jhoon.han@outlook.com>
2024-04-10 00:20:19 +0100

    good day

---
+++ 125_validPalindrome/index2.py
@@ -0,0 +1,21 @@
+import re
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = re.sub(r"[^a-zA-Z0-9]", "", s)
+        s = "".join(s)
+        s = s.lower()
+        l = 0
+        r = len(s) - 1
+
+        while l <= r:
+            if s[l] != s[r]:
+                return False
+            l += 1
+            r -= 1
+
+        return True
+
+
+Solution().isPalindrome("A man, a plan, a canal: Panama")
