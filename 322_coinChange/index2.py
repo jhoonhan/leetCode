86e1132fb4a7b01738e5f00c524a6cebd7de024b
Joe Han <jhoon.han@outlook.com>
2024-04-10 00:15:39 +0100

    getting good at dfs

---
+++ 322_coinChange/index2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def coinChange(self, coins: list, amount: int) -> int:
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+
+        for i in range(1, amount + 1):
+            for c in coins:
+                if i - c >= 0:
+                    dp[i] = min(dp[i], dp[i - c] + 1)
+
+        print(dp)
+        return dp[amount] if dp[amount] != amount + 1 else -1
+
+
+Solution().coinChange([2], 3)
