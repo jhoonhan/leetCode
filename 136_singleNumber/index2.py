009017b554a17d3e6ea9e8a3f15ce88e2e67dd18
Joe Han <jhoon.han@outlook.com>
2024-04-09 15:54:54 +0100

    what the fuck

---
+++ 136_singleNumber/index2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def singleNumber(self, nums: list) -> int:
+        print("aang")
+
+
+num = 3
+i = 0
+
+
+# print((num >> i) & 1)
+ans = 0
+bit_sum = 1
+res = bit_sum % 3
+ans |= bit_sum << 0
+
+twos = 0
+# print(bin(twos))
+# print(bin(~twos))
+
+
+# print(~twos)
+print(2 & ~3)
--- 502_IPO/index.py
+++ 502_IPO/index.py
@@ -3,7 +3,19 @@ import heapq
 
 class Solution:
     def findMaximizedCapital(self, k: int, w: int, profits: list, capital: list) -> int:
-        print("aang")
+        maxProfit = []
+        minCapital = [(c, p) for c, p in zip(capital, profits)]
+        heapq.heapify(minCapital)
+
+        for i in range(k):
+            while minCapital and minCapital[0][0] <= w:
+                c, p = heapq.heappop(minCapital)
+                heapq.heappush(maxProfit, -1 * p)
+
+            if not maxProfit:
+                break
+            w += -1 * heapq.heappop(maxProfit)
+        return w
 
 
 Solution().findMaximizedCapital(2, 0, [1, 2, 3], [0, 1, 1])
