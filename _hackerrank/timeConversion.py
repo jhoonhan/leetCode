60248cef8ea64547eb8e64b8b83f6d85d709cbef
Joe Han <jhoon.han@outlook.com>
2024-04-10 11:51:07 +0100

    working

--- _hackerrank/timeConversion.py
+++ _hackerrank/timeConversion.py
@@ -13,6 +13,24 @@ import sys
 # The function accepts STRING s as parameter.
 #
 
+
 def timeConversion(s):
     # Write your code here
+    ampm = s[len(s) - 2 :]
+    splitted = s[:-2].split(":")
+    h = int(splitted[0])
+    m = splitted[1]
+    s = splitted[2]
 
+    if ampm == "AM" and h == 12:
+        h = 0
+
+    if ampm == "PM" and h < 12:
+        h += 12
+
+    f_h = "{:02}".format(h)
+    print(f"{f_h}:{m}:{s}")
+    return f"{f_h}:{m}:{s}"
+
+
+timeConversion("12:05:45AM")
