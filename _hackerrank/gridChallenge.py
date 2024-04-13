15cde9c65e3f6c72781706da699a785d6b8f34d8
Joe Han <jhoon.han@outlook.com>
2024-04-13 15:07:33 +0100

    leggo

--- _hackerrank/gridChallenge.py
+++ _hackerrank/gridChallenge.py
@@ -13,5 +13,27 @@ import sys
 # The function accepts STRING_ARRAY grid as parameter.
 #
 
+
 def gridChallenge(grid):
     # Write your code here
+    for i, row in enumerate(grid):
+        arr = list(row)
+        arr.sort()
+        grid[i] = "".join(arr)
+
+    cols = []
+    for c in range(len(grid[0])):
+        res = []
+        for r in range(len(grid)):
+            res.append(grid[r][c])
+        cols.append(res)
+
+    for arr in cols:
+        if arr != sorted(arr):
+            print(f"failed: {arr}")
+            return "NO"
+
+    return "YES"
+
+
+gridChallenge(["acb", "dfe", "cdf"])
