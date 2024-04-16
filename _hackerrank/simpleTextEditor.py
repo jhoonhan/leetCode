25a604db59ae794bdb85e77c59bb133af6e8815d
Joe Han <jhoon.han@outlook.com>
2024-04-16 23:14:15 +0100

    leggo

--- _hackerrank/simpleTextEditor.py
+++ _hackerrank/simpleTextEditor.py
@@ -1,33 +1,44 @@
-import collections
-
-
 class Editor:
     def __init__(self) -> None:
         self.s1 = []
-        self.s2 = collections.deque()
+        self.s2 = []
 
     def append(self, w):
         for char in w:
             self.s1.append(char)
-        self.s2.append(("append", len(w)))
+        self.s2.append(("append", w))
+        print(f"Appended: {w}")
+        print(self.s1)
+        print("")
 
     def delete(self, k):
-        popped = ""
+        popped = []
         for _ in range(k):
             if not self.s1:
-                return
-            popped = self.s1.pop() + popped
-        self.s2.append(("delete", popped))
+                break
+            popped.append(self.s1.pop())
+        self.s2.append(("delete", "".join(popped[::-1])))
+        print(f"Deleted: {popped[::-1]}")
+        print(f"{self.s1}")
+        print("")
 
-    def print(self):
-        print("".join(self.s1))
+    def print(self, k):
+        if k > len(self.s1):
+            return
+        print(self.s1[k - 1])
 
     def undo(self):
         action, value = self.s2.pop()
+        print("undoing:", action, value)
+        print(self.s2)
+
+        # print(self.s2)
+
         if action == "append":
-            self.delete(value)
+            self.delete(len(value))
         elif action == "delete":
-            self.s1.append(value)
+            self.append(value)
+        print("")
 
 
 # if __name__ == "__main__":
@@ -39,25 +50,21 @@ class Editor:
 #         commands = data.split(" ")
 
 #         if commands[0] == "1":
-#             editor.append(commands[0])
+#             editor.append(commands[1])
 #         elif commands[0] == "2":
 #             editor.delete(int(commands[1]))
 #         elif commands[0] == "3":
-#             print("leggo")
-
-#             editor.print()
+#             editor.print(int(commands[1]))
 #         elif commands[0] == "4":
 #             editor.undo()
 
 editor = Editor()
 
 editor.append("abc")
-
-editor.delete(2)
-editor.print()
-
+editor.print(3)
+editor.delete(3)
+editor.append("xy")
+editor.print(2)
 editor.undo()
-editor.print()
-
-editor.delete(1)
-editor.print()
+editor.undo()
+editor.print(1)
