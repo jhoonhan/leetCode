7d336533c9f8ab21e3b968cd420bb411010ae0c7
Joe Han <jhoon.han@outlook.com>
2024-04-16 23:42:32 +0100

    52 O(n) approaceh

--- 55_jumpGame/index.py
+++ 55_jumpGame/index.py
@@ -1,6 +1,33 @@
 class Solution:
+    # def canJump(self, nums) -> bool:
+    #     dp = [0] * len(nums)
+    #     dp[-1] = nums[-1]
+
+    #     for i in range(len(nums) - 1, -1, -1):
+    #         # if dp[i]:
+    #         #     continue
+
+    #         max_val = 0
+    #         for j in range(nums[i] + 1):
+    #             index = i + j
+    #             if index in range(len(nums)):
+    #                 # print(f"max:{max_val}, {dp[index]}, {i}")
+    #                 max_val = max(max_val, dp[index], i)
+    #         dp[i] = max_val
+
+    #     res = dp[0] >= len(nums) - 1
+    #     print(res)
+
+    #     return res
+
     def canJump(self, nums) -> bool:
-        print("aang")
+        goal = len(nums) - 1
+
+        for i in range(len(nums) - 1, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i
+
+        return goal == 0
 
 
-Solution().canJump([2, 3, 1, 1, 4])
+Solution().canJump([3, 2, 1, 0, 4])
