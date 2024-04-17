1410e5306a6c2df388e076959075764943942fa6
Joe Han <jhoon.han@outlook.com>
2024-04-18 00:08:02 +0100

    third attempt

---
+++ 120_triangle/index3.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimumTotal(self, triangle: list) -> int:
+        dp = [] * len(triangle)
+
+        print(dp)
+
+
+Solution().minimumTotal([[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]])
