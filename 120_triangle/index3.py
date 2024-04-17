116e541a4ea6b7dbd859a3d909cfdd8d22ab5a3d
Joe Han <jhoon.han@outlook.com>
2024-04-18 00:18:01 +0100

    good job

--- 120_triangle/index3.py
+++ 120_triangle/index3.py
@@ -1,8 +1,12 @@
 class Solution:
     def minimumTotal(self, triangle: list) -> int:
-        dp = [] * len(triangle)
+        dp = [0] * (len(triangle) + 1)
 
-        print(dp)
+        for i in range(len(triangle) - 1, -1, -1):
+            for j in range(len(triangle[i])):
+                dp[j] = min(dp[j], dp[j + 1]) + triangle[i][j]
+
+        return dp[0]
 
 
 Solution().minimumTotal([[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]])
