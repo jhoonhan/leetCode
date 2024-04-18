aaa3ee8bbfc896736e7e5afec7435712bbff6934
Joe Han <jhoon.han@outlook.com>
2024-04-19 00:04:29 +0100

    wtf

---
+++ 97_interleavingString/index2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        print("aang")
+
+        return False
+
+
+Solution().isInterleave("aabcc", "dbbca", "aadbbcbcac")
