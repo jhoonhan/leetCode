b3c19ded830cfd49d499072a06f7515de603d790
Joe Han <jhoon.han@outlook.com>
2024-04-18 23:05:43 +0100

    man im getting good

--- 274_hIndex/index.py
+++ 274_hIndex/index.py
@@ -1,3 +1,18 @@
 class Solution:
-    def hIndex(self, citations: List[int]) -> int:
-        
+    def hIndex(self, citations: list) -> int:
+        citations.sort()
+        print(citations)
+        length = len(citations)
+
+        res = 0
+        for i, n in enumerate(citations):
+            v1 = n
+            v2 = length - i
+            v3 = min(v1, v2)
+            res = max(res, v3)
+
+        print(res)
+        return res
+
+
+Solution().hIndex([1, 3, 1])
