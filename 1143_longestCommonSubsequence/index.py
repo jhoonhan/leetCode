a0785d4a063e19c38a1fb0f93d3c7e5d2a117829
Joe Han <jhoon.han@outlook.com>
2024-04-19 23:45:40 +0100

    im not smart enough

--- 1143_longestCommonSubsequence/index.py
+++ 1143_longestCommonSubsequence/index.py
@@ -1,5 +1,16 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        
+        dp = [[0 for _ in range(len(text2) + 1)] for _ in range(len(text1) + 1)]
+
+        for r in range(len(text1) - 1, -1, -1):
+            for c in range(len(text2) - 1, -1, -1):
+                cur1 = text2[c]
+                cur2 = text1[r]
+                if cur1 == cur2:
+                    dp[r][c] = 1 + dp[r + 1][c + 1]
+                else:
+                    dp[r][c] = max(dp[r + 1][c], dp[r][c + 1])
+        return dp[0][0]
+
 
 Solution().longestCommonSubsequence("abcde", "ace")
