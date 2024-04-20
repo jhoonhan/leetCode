798c606a116f588edb16dae78ff60b23d636351c
Joe Han <jhoon.han@outlook.com>
2024-04-20 23:51:50 +0100

    iterative

--- 173_binarySearchTreeIterator/index.py
+++ 173_binarySearchTreeIterator/index.py
@@ -7,32 +7,23 @@
 class BSTIterator:
 
     def __init__(self, root: Optional[TreeNode]):
-        self.root = TreeNode(-1)
-        self.counter = 0
-        self.traversed = [self.root]
-        self.dfs(root)
+        self.stack = []
 
-    def dfs(self, node):
-
-        if node == None:
-            return None
-
-        self.dfs(node.left)
-        self.traversed.append(node)
-        self.dfs(node.right)
+        while root:
+            self.stack.append(root)
+            root = root.left
 
     def next(self) -> int:
-        self.counter += 1
-        if self.counter not in range(len(self.traversed)):
-            return False
-        self.root = self.traversed[self.counter]
-        return self.root.val
+        res = self.stack.pop()
+        cur = res.right
+        while cur:
+            self.stack.append(cur)
+            cur = cur.left
+
+        return res.val
 
     def hasNext(self) -> bool:
-        if self.counter < len(self.traversed) - 1:
-            return True
-        else:
-            return False
+        return self.stack != []
 
 
 # Your BSTIterator object will be instantiated and called as such:
