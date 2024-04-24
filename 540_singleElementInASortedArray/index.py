8e6db6017752b35668a40097e7606ab50c57a425
Joe Han <jhoon.han@outlook.com>
2024-04-24 11:34:16 +0100

    good job joe

--- 540_singleElementInASortedArray/index.py
+++ 540_singleElementInASortedArray/index.py
@@ -1,11 +1,33 @@
 class Solution:
     def singleNonDuplicate(self, nums: list) -> int:
-        res = 0
-        for n in nums:
-            res = n ^ res
-        print(res)
+        if len(nums) == 1:
+            return nums[0]
+        l = 0
+        r = len(nums) - 1
 
-        return res
+        while l <= r:
+            m = l + ((r - l) // 2)
+
+            if m - r <= 3:
+                res = 0
+                for n in nums[l : r + 1]:
+                    res = n ^ res
+                return res
+
+            if nums[m] != nums[m - 1] and nums[m] != nums[m + 1]:
+                return nums[m]
+
+            # if index is even
+            if m % 2 == 0:
+                if nums[m - 1] == nums[m]:
+                    r = m
+                else:
+                    l = m
+            else:
+                if nums[m - 1] == nums[m]:
+                    l = m
+                else:
+                    r = m
 
 
-Solution().singleNonDuplicate([1, 1, 2, 3, 3, 4, 4, 8, 8])
+Solution().singleNonDuplicate([1, 1, 2])
