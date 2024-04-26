98cb4d0092c092b382bde81651ea8c4bcb40b199
Joe Han <jhoon.han@outlook.com>
2024-04-26 22:48:47 +0100

    aang

--- 521_longestUncommonSubsequenceI/index.py
+++ 521_longestUncommonSubsequenceI/index.py
@@ -1,3 +1,13 @@
 class Solution:
     def findLUSlength(self, a: str, b: str) -> int:
-        
+        if a == b:
+            return -1
+
+        return max(len(a), len(b))
+
+
+Solution().findLUSlength("aba", "cdc")
+
+#
+#
+#
