40d5231a54cb1962fca216bb6e6d147d7fcf7978
Joe Han <jhoon.han@outlook.com>
2024-05-21 11:07:24 +0100

    hmm

---
+++ 117_populatingNextRightPointerInEachNodeII/index2.py
@@ -0,0 +1,36 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+
+class Solution:
+    def connect(self, root):
+        if root == None:
+            return None
+        q = collections.deque()
+        q.append(root)
+        while q:
+            q_len = len(q)
+            res = []
+            for _ in range(q_len):
+                cur = q.popleft()
+                if cur.left:
+                    q.append(cur.left)
+                    res.append(cur.left)
+                if cur.right:
+                    q.append(cur.right)
+                    res.append(cur.right)
+            if res:
+                for i in range(len(res)):
+                    if i + 1 < len(res):
+                        res[i].next = res[i + 1]
+                    else:
+                        res[i].next = None
+
+        return root
