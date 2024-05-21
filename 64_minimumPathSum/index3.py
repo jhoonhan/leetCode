964dc0ff01d6e5f0d2540a47a56a4bae3260a78f
Joe Han <jhoon.han@outlook.com>
2024-05-21 10:55:45 +0100

    yes

--- 64_minimumPathSum/index3.py
+++ 64_minimumPathSum/index3.py
@@ -1,6 +1,21 @@
 class Solution:
     def minPathSum(self, grid) -> int:
-        print("aang")
+        row = len(grid)
+        col = len(grid[0])
+        dp = [[float("inf")] * (col + 1) for _ in range(row + 1)]
+
+        for r in range(row - 1, -1, -1):
+            for c in range(col - 1, -1, -1):
+                right = dp[r][c + 1]
+                bottom = dp[r + 1][c]
+                value = min(right, bottom)
+
+                if value == float("inf"):
+                    dp[r][c] = grid[r][c]
+                else:
+                    dp[r][c] = value + grid[r][c]
+
+        return dp[0][0]
 
 
 Solution().minPathSum([[1, 3, 1], [1, 5, 1], [4, 2, 1]])
