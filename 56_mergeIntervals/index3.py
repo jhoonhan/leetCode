253baad51318928978c81873be589b75abe837b1
Joe Han <jhoon.han@outlook.com>
2024-05-24 18:49:52 +0100

    wasnt bad

--- 56_mergeIntervals/index3.py
+++ 56_mergeIntervals/index3.py
@@ -1,7 +1,25 @@
 class Solution:
     def merge(self, intervals):
-        for el in intervals:
-            print(el)
+        res = []
+        sorted_list = sorted(intervals, key=lambda x: x[0])
+
+        start = sorted_list[0][0]
+        end = sorted_list[0][1]
+
+        for el in sorted_list:
+            el_start = el[0]
+            el_end = el[1]
+
+            if el_start <= end:
+                start = min(start, el_start)
+                end = max(end, el_end)
+            else:
+                res.append([start, end])
+                start = el_start
+                end = el_end
+        res.append([start, end])
+
+        return res
 
 
-Solution().merge([[1, 3], [2, 6], [8, 10], [15, 18]])
+Solution().merge([[1, 4], [0, 0]])
