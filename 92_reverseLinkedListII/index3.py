2a759a5f9805592426b275f97d2475c32bf2a292
Joe Han <jhoon.han@outlook.com>
2024-05-30 10:53:11 +0100

    lets go

--- 92_reverseLinkedListII/index3.py
+++ 92_reverseLinkedListII/index3.py
@@ -5,3 +5,34 @@
 #         self.next = next
 class Solution:
     def reverseBetween(self, head, left: int, right: int):
+        dummy = ListNode(0, head)
+
+        counter = 0
+
+        node_before_reverse = None
+        new_tail = None
+
+        before = dummy
+        temp = head
+        after = head
+
+        while after:
+            counter += 1
+            after = after.next
+
+            if counter == left:
+                node_before_reverse = before
+                before.next = None
+                new_tail = temp
+
+            if left < counter <= right:
+                temp.next = before
+
+            if counter == right:
+                new_tail.next = after
+                node_before_reverse.next = temp
+
+            before = temp
+            temp = after
+
+        return dummy.next
