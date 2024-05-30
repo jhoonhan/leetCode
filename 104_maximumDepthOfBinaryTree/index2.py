ba0608f47cde7e5d56af9f2b8cb76cc10c33c9f7
Joe Han <jhoon.han@outlook.com>
2024-05-30 11:00:17 +0100

    dfs with counter

--- 104_maximumDepthOfBinaryTree/index2.py
+++ 104_maximumDepthOfBinaryTree/index2.py
@@ -6,4 +6,20 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root) -> int:
-        
+        depth = 0
+
+        def dfs(node, counter):
+            if node == None:
+                return
+
+            counter += 1
+
+            dfs(node.left, counter)
+            dfs(node.right, counter)
+
+            nonlocal depth
+            depth = max(depth, counter)
+
+        dfs(root, depth)
+
+        return depth
