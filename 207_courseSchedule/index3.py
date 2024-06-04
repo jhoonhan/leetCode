5de5ce4a88d74a5c2a3abb50d2c85c385e792aa1
Joe Han <jhoon.han@outlook.com>
2024-06-04 13:21:14 +0100

    hmm

--- 207_courseSchedule/index3.py
+++ 207_courseSchedule/index3.py
@@ -3,11 +3,35 @@ from collections import defaultdict
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites) -> bool:
-        adj_list = defaultdict(list)
+        adj = defaultdict(list)
         for el in prerequisites:
-            adj_list[el[0]].append(el[1])
+            adj[el[0]].append(el[1])
 
-        print(adj_list)
+        visited = set()
+
+        def dfs(course):
+            if course in visited:
+                return False
+            if adj[course] == []:
+                return True
+
+            visited.add(course)
+            for preq in adj[course]:
+                res = dfs(preq)
+                if not res:
+                    return False
+
+            visited.remove(course)
+            adj[course] = []
+
+            return True
+
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+
+        return True
 
 
-Solution().canFinish(2, [[0, 1], [0, 2], [1, 3], [1, 4], [3, 4]])
+# Solution().canFinish(2, [[0, 1], [0, 2], [1, 3], [1, 4], [3, 4]])
+Solution().canFinish(2, [[0, 1], [1, 0]])
