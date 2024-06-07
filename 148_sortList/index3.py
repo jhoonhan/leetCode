8c73babac459ba6cc1783add030176e90db0dd2a
Joe Han <jhoon.han@outlook.com>
2024-06-07 18:52:42 +0100

    selection sort

--- 148_sortList/index3.py
+++ 148_sortList/index3.py
@@ -19,3 +19,24 @@ class Solution:
             sorted_until = current
 
         return head
+
+    def sortList(self, head):
+        print("god is good")
+        if head == None or head.next == None:
+            return head
+
+        temp = head
+        while temp.next:
+            min_node = temp
+            curr = temp.next
+
+            while curr:
+                if curr.val < min_node.val:
+                    min_node = curr
+                curr = curr.next
+
+            if curr != min_node:
+                temp.val, min_node.val = min_node.val, temp.val
+
+            temp = temp.next
+        return head
