038a515434f7620f6ae58e76226e8beffbc05601
Joe Han <jhoon.han@outlook.com>
2024-06-08 12:30:16 +0100

    binary serach

---
+++ 35_searchInsertPosition/index3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def searchInsert(self, nums, target: int) -> int:
+        l = 0
+        r = len(nums) - 1
+
+        while l <= r:
+            m = l + ((r - l) // 2)
+
+            if nums[m] == target:
+                return m
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+
+        return l
