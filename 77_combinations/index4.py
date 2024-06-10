4b94ac9217ef14315d86ffbf8bc91877bab5aa34
Joe Han <jhoon.han@outlook.com>
2024-06-10 22:20:17 +0100

    combo

--- 77_combinations/index4.py
+++ 77_combinations/index4.py
@@ -1,6 +1,19 @@
 class Solution:
     def combine(self, n: int, k: int) -> list:
-        
+        res = []
+
+        def backtracK(i, acc):
+            if len(acc) == k:
+                res.append(acc)
+                return
+
+            for j in range(i, n):
+                backtracK(j + 1, acc + [j + 1])
+
+        backtracK(0, [])
+
+        print(res)
+        return res
 
 
 Solution().combine(4, 2)
