b624a4b06d84ae82d7b9ba7db06819cd63f0650c
Joe Han <jhoon.han@outlook.com>
2024-06-10 22:18:22 +0100

    lets go

--- 162_findPeakElement/index3.py
+++ 162_findPeakElement/index3.py
@@ -7,7 +7,7 @@ class Solution:
             m = l + ((r - l) // 2)
             if m > 0 and nums[m] < nums[m - 1]:
                 r = m - 1
-            elif m < len(nums) and nums[m] < nums[m + 1]:
+            elif m < len(nums) - 1 and nums[m] < nums[m + 1]:
                 l = m + 1
             else:
                 return m
--- 77_combinations/index4.py
+++ 77_combinations/index4.py
@@ -1,17 +1,6 @@
 class Solution:
     def combine(self, n: int, k: int) -> list:
-        res = []
-
-        def backtrack(i, acc):
-            if len(acc) == k:
-                res.append(acc[:])
-                return
-            for j in range(i, n):
-                backtrack(j + 1, acc + [j + 1])
-
-        backtrack(0, [])
-
-        print(res)
+        
 
 
 Solution().combine(4, 2)
